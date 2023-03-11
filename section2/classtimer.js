ef5d35bc8509bb4b820731cad0c11558dc26e988
kahtab1324 <k.s.fayyad.dev@gmail.com>
2023-03-11 23:09:38 +0200

    add Timer class. Refactor code. add style.

---
+++ section2/classtimer.js
@@ -0,0 +1,58 @@
+class Timer {
+  constructor(dutrationInput, startButton, pauseButton, callbacks) {
+    this.dutrationInput = dutrationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    if (callbacks) {
+      // i but it in if statment because callback is optional if callbacks is exist enter inside the condition
+      this.onStart = callbacks.onStart; //because onStart is object
+      this.onTick = callbacks.onTick;
+      this.onAlmostComplete = callbacks.onAlmostComplete;
+      this.onComplete = callbacks.onComplete;
+    }
+    startButton.addEventListener("click", this.start);
+    pauseButton.addEventListener("click", this.pause);
+  }
+  start = () => {
+    this.tick();
+    this.tinterval = setInterval(this.tick, 50);
+    if (this.onStart) {
+      //you check if there onStart because rememeber it is optional ,if there is no callbacks and you don't provide check it will give you error becuase it not deffine
+      this.onStart();
+    }
+    // clearInterval(timer);
+  };
+
+  pause = () => {
+    clearInterval(this.tinterval);
+    console.log("you are paused");
+  };
+
+  tick = () => {
+    let timeRemaining = this.timeRemaining;
+    if (timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining = timeRemaining - 0.05; // becuse set timeRemaining(time) , and remember timeRemaining is a function watch epsoede 207
+    }
+    if (this.onTick) {
+      this.onTick();
+    }
+    if (timeRemaining === 10) {
+      if (this.onAlmostComplete) {
+        this.onAlmostComplete();
+      }
+    }
+  };
+  // get is key use inside class and you can deal with function like variable without ()
+  // and the propose for use it to make the syntax easy and trick the other engeneers
+  get timeRemaining() {
+    return parseFloat(this.dutrationInput.value);
+  }
+  set timeRemaining(time) {
+    this.dutrationInput.value = time.toFixed(2);
+  }
+}
--- section2/timer.js
+++ section2/timer.js
@@ -1,29 +1,24 @@
 const dutrationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.getElementById("pause");
+const circle = document.querySelector("circle");
+const perimeter = circle.getAttribute("r") * Math.PI * 2;
+circle.setAttribute("stroke-dasharray", perimeter);
 
-class Timer {
-  constructor(dutrationInput, startButton, pauseButton) {
-    this.dutrationInput = dutrationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    startButton.addEventListener("click", this.start);
-    pauseButton.addEventListener("click", this.pause);
-  }
-  start = () => {
-    this.tick();
-    this.tinterval = setInterval(this.tick, 1000);
-    // clearInterval(timer);
-  };
-
-  pause = () => {
-    clearInterval(this.tinterval);
-    console.log("you are paused");
-  };
-
-  tick = () => {
-    console.log("here i am");
-  };
-}
-const timer = new Timer(dutrationInput, startButton, pauseButton);
+let currentofset = 0;
+const timer = new Timer(dutrationInput, startButton, pauseButton, {
+  onStart() {
+    console.log("run the timer is started");
+  },
+  onTick() {
+    console.log("the timer is running");
+    circle.setAttribute("stroke-dashoffset", currentofset);
+    currentofset = currentofset - 2;
+  },
+  onAlmostComplete() {
+    console.log("there is 10s remaining");
+  },
+  onComplete() {
+    console.log("we finely finesh");
+  },
+});
