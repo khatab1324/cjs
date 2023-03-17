be565324048bf39480e1400d045e1a505997af18
kahtab1324 <k.s.fayyad.dev@gmail.com>
2023-03-17 12:37:33 +0200

    make onSelect file and code

--- section2/design-patterns/movies+-+003+-+starter+kit/index.js
+++ section2/design-patterns/movies+-+003+-+starter+kit/index.js
@@ -1,9 +1,9 @@
 console.log("Hi there!");
 const input = document.querySelector("input");
 
-// it wait user to finsh write and
+// it wait user to finsh write becuase we have 1000 req
 let timeoutId;
-function whenPress() {
+async function whenPress() {
   if (timeoutId) {
     //see timeoutId define
     clearTimeout(timeoutId); //if your clearTimeout when setTimeout run and wait the clearTimeout will stop it from work
@@ -12,6 +12,17 @@ function whenPress() {
     //now its waiting one sec for next key and before call fetchData if the user enter next key ,the timeoutId define and will enter inside if statment and clear the timeoutId and so on
     fetchData(input.value);
   }, 1000);
+
+  for (let movie of movies) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <img src="${movie.Poster}"/>
+    <h1>${movie.Title}</h1>
+    `; //we need double quote "${}"because we need the img not the text if you don't put the ""you will find https://sdfkjsldfjlsdkfj
+    const collect = document.querySelector("#collect");
+    collect.appendChild(div);
+  }
+  const movie = await fetchData(input.value);
 }
 
 input.addEventListener("input", whenPress);
@@ -20,6 +31,13 @@ async function fetchData(input) {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: { apikey: "97f9707", s: input },
   }); //we write it like pram and axios will do it job thank axios . rether than write it like this  "http://www.omdbapi.com/?apikey=97f9707&s=dark"
-  console.log(response.data);
+
+  //   now if you search about something that not exist (you will find in network on preview for your rquast)
+  if (response.data.Error) {
+    //that mean if there error
+    console.log("there no match");
+    return [];
+  }
+  return response.data.Search;
 }
 // my key : 97f9707
