8893afa475c35e2cca2b941dc28a8aa080ab8bc3
kahtab1324 <k.s.fayyad.dev@gmail.com>
2023-04-16 10:09:40 +0300

    make sing in form

--- section2/e-commerce/userpage/appuser.js
+++ section2/e-commerce/userpage/appuser.js
@@ -1,1 +1,21 @@
+const express = require("express");
+const app = express();
+app.use(express.static(__dirname));
+// app.use(express.static("public"));
+// app.use(express.static("files"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/" + "main.html");
+});
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/" + "navbar.css");
+});
+app.get("/sign-in", (req, res) => {
+  res.sendFile(__dirname + "/" + "sign-in.html");
+});
+app.post("/sign-in", (req, res) => {
+  res.send("Account created");
+});
 
+app.listen(2000, () => {
+  console.log("ah i listening");
+});
